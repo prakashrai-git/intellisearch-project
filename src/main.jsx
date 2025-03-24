1653f33a424c140522d1fe50ae7e18f8dc9092f9
Prakash Rai <prakashrai3446@gmail.com>
2025-03-24 16:11:15 +0530

    Added environtment variables for auth0

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import ContextProvider from './context/Context.jsx'
 import { Auth0Provider } from '@auth0/auth0-react';
+const authConfig = {
+  domain: import.meta.env.VITE_AUTH0_DOMAIN,
+  clientId: import.meta.env.VITE_AUTH0_CLIENT_ID,
+};
+
 createRoot(document.getElementById('root')).render(
 <ContextProvider>
 <Auth0Provider
-    domain="dev-w558astza2ur7okd.us.auth0.com"
-    clientId="mBRJ3PvRYfCQdbNBc7OOXgcjzUkUyv1C"
+    // domain="dev-w558astza2ur7okd.us.auth0.com"
+    domain={authConfig.domain}
+    clientId={authConfig.clientId}
+    // clientId="mBRJ3PvRYfCQdbNBc7OOXgcjzUkUyv1C"
+    
     authorizationParams={{
       redirect_uri: window.location.origin
     }}
