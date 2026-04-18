e0ec41d16135c591e6f4ce4bedc8b6ef067df00a
prakashrai-git <141764063+prakashrai-git@users.noreply.github.com>
2026-04-18 21:44:11 +0530

    Update user.js

--- user.js
+++ user.js
@@ -1,22 +1,22 @@
 import { MongoClient } from "mongodb";
-import dotenv from "dotenv";
-
-dotenv.config(); // Load environment variables
-
-const uri = process.env.MONGO_URI;
+//const uri="mongodb+srv://amarjeet34537:DmH9JrGr46K5Qwhv@cluster0.2cp2m.mongodb.net/"
+const uri="mongodb+srv://amarjeet34537:DmH9JrGr46K5Qwhv@cluster0.2cp2m.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 const client = new MongoClient(uri, {
     serverSelectionTimeoutMS: 5000 // 5 seconds
 });
-const dbName = process.env.DB_NAME;
+//const client = new MongoClient(url);
+const dbName = 'intelli_search';
 
-await client.connect();
+await client.connect(); // Use await with async functions
 const db = client.db(dbName);
 
+
+// user.js
 export async function update_chat_data(req, res) {
     try {
         const username = req.query.username;
         const chatMessage = req.query.chat;
-
+        console.log()
         await db.collection('chat_data').updateOne(
             { username: username },
             {
@@ -34,12 +34,14 @@ export async function update_chat_data(req, res) {
 
 export async function get_chat_data(req, res) {
     try {
-        const username = req.query.username;
+        const username = req.query.username
+        // Find the chat data for the specified username
         const chatData = await db.collection('chat_data').findOne(
             { username: username },
-            { projection: { chat: 1, _id: 0 } }
+            { projection: { chat: 1, _id: 0 } } // Only return the chat array, exclude the _id field
         );
 
+        // Check if chat data exists for the username
         if (chatData) {
             res.send({ 'chat': chatData.chat });
         } else {
@@ -49,4 +51,5 @@ export async function get_chat_data(req, res) {
         console.log(e);
         res.send({ 'message': JSON.stringify(e) });
     }
+
 }
