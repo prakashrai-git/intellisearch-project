09dc0c2eef06a81719975306e210426eb51cdcc7
prakashrai-git <141764063+prakashrai-git@users.noreply.github.com>
2026-04-19 10:17:52 +0530

    Update gemini.jsx

--- src/config/gemini.jsx
+++ src/config/gemini.jsx
@@ -10,7 +10,7 @@ const apiKey = import.meta.env.VITE_GEMINI_API_KEY;
 const genAI = new GoogleGenerativeAI(apiKey);
 
 const model = genAI.getGenerativeModel({
-  model: "gemini-3.1-flash-lite",
+  model: "gemini-3-flash"
 });
 
 const generationConfig = {
